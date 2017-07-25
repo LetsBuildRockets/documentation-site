a836eca2ecf5c0546bdb2ab2c153c55eac3bc314
TJScalzo <timtv25@gmail.com>
2017-07-24 20:41:57 -0400

    Added taggle for adding tags to articles
    (and eventually files)
    #1

--- components/createArticle.js
+++ components/createArticle.js
@@ -1,17 +1,44 @@
-const createArticle = () => (
-  <div>
-  <form>
-    Title: <input type="text" name="title" placeholder="Untitled"></input><br></br>
-    Author: <input type="text" name="author" placeholder="You"></input> <button type="button" onclick="alert('Pretend this adds another field for an author.')">Add Author</button><br></br>
-    Content: <textarea name="content" rows="10" placeholder="Markdown Available"></textarea><br></br>
-    Abstract: <textarea name="abstract" rows="2" placeholder="Markdown Available"></textarea><br></br>
-    Thumbnail: <input type="text" name="thumbnail" placeholder="(Link to a photo)"></input><br></br>
-    Project Page? <input type="radio" name="article" value="Yes"></input>Yes <input type="radio" name="article" value="No"></input>No<br></br>
+export default class extends React.Component {
+  render () {
+    return (
+      <div>
+        <form id="article">
+          Title: <input type="text" name="title" placeholder="Untitled"></input><br />
+          Author: <input type="text" name="author" placeholder="You"></input> <button id="addAuthor" type="button">Add Author</button><br />
+          Content: <textarea name="content" rows="10" placeholder="Markdown Available"></textarea><br />
+          Abstract: <textarea name="abstract" rows="2" placeholder="Markdown Available"></textarea><br />
+          Thumbnail: <input type="text" name="thumbnail" placeholder="(Link to a photo)"></input><br />
+          Project Page? <input type="radio" name="article" value="Yes"></input>Yes <input type="radio" name="article" value="No"></input>No<br />
+          Tags: <div id="tags"></div><br />
+          Needed/Related Tags: <div id="needed_tags"></div><br />
 
+          <input type="submit" value="Submit"></input>
+        </form>
+      </div>
+    )
+  }
+  componentDidMount() {
+    const script1 = document.createElement("script");
+    const script2 = document.createElement("script");
+    const script3 = document.createElement("script");
+    const script4 = document.createElement("script");
+    const style1 = document.createElement("link");
 
-    <input type="submit" value="Submit"></input>
-  </form>
-  </div>
-)
-needed_tags, tags, url_slug
-export default createArticle
+    script1.src = "https://code.jquery.com/jquery-1.10.1.min.js";
+
+    script2.src = "/scripts/jquery-ui-1.10.4.custom.min.js";
+
+    script3.src = "/scripts/taggle.js";
+
+    script4.src = "/scripts/createArticleScript.js";
+
+    style1.href = "styles/taggle.css";
+    style1.rel = "stylesheet";
+    style1.type = "text/css";
+
+    document.body.appendChild(script1);
+    document.body.appendChild(script3);
+    document.body.appendChild(script4);
+    document.body.appendChild(style1);
+  }
+}
---
+++ static/scripts/createArticleScript.js
@@ -0,0 +1,4 @@
+var tags = new Taggle("tags");
+var needed_tags = new Taggle("needed_tags");
+
+document.getElementById('addAuthor').onclick = function() {alert('Pretend this adds another field for an author.')};
