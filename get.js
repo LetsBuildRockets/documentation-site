9964dd3b701d0a1d82a84e9f4c2772f0ee5c8046
Eric Sims <ericsimsm@gmail.com>
2017-07-25 19:05:47 -0400

    get requests to google api

---
+++ get.js
@@ -0,0 +1,22 @@
+const https = require('https');
+
+const options = {
+  hostname: 'www.googleapis.com',
+  port: 443,
+  path: '/drive/v2/files/0BwFaRrEmPx2yNk9QdFFCZ0hCbzQ?key=KEYGOESHERE',
+  method: 'GET',
+};
+
+const req = https.request(options, (res) => {
+  console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+
+  res.on('data', (d) => {
+    process.stdout.write(d);
+  });
+});
+
+req.on('error', (e) => {
+  console.error(e);
+});
+req.end();
