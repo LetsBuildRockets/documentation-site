ef50e7755a50d5ebc19643405abd80b36ffa6196
TJScalzo <timtv25@gmail.com>
2017-07-24 20:42:21 -0400

    Indentation

--- components/createUser.js
+++ components/createUser.js
@@ -1,13 +1,13 @@
 const createUser = () => (
   <div>
-  <form>
-    First name: <input type="text" name="first_name" placeholder="Jack"></input><br></br>
-    Last name: <input type="text" name="last_name" placeholder="Doe"></input><br></br>
-    Username: <input type="text" name="username" placeholder="jackyd"></input><br></br>
-    Profile Picture: <input type="text" name="profilepicture" placeholder="(Link to a photo)"></input><br></br>
-    Existing article? <input type="radio" name="article" value="Yes"></input>Yes <input type="radio" name="article" value="No"></input>No<br></br>
-    <input type="submit" value="Submit"></input>
-  </form>
+    <form id="user" hidden>
+      First name: <input type="text" name="first_name" placeholder="Jack"></input><br />
+      Last name: <input type="text" name="last_name" placeholder="Doe"></input><br />
+      Username: <input type="text" name="username" placeholder="jackyd"></input><br />
+      Profile Picture: <input type="text" name="profilepicture" placeholder="(Link to a photo)"></input><br />
+      Existing article? <input type="radio" name="article" value="Yes"></input>Yes <input type="radio" name="article" value="No"></input>No<br />
+      <input type="submit" value="Submit"></input>
+    </form>
   </div>
 )
 
