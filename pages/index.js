f5a53e174e8d33006c3e0c7d2348a7d168d60100
Eric Sims <EricSimsM@gmail.com>
2019-09-02 01:14:44 -0600

    fixed window.location.host reference......

--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,35 @@ import Layout from '../components/mainLayout.js'
 import Link from 'next/link'
 import fetch from 'isomorphic-unfetch'
 
-const Index = (props) => (
-  <Layout>
-    <h1>Articles</h1>
-    <ul>
-      {props.articles.map((article) => (
-        <li key={article.id}>
-          <Link as={`/a/${article.url_slug}`} href={`/article?slug=${article.url_slug}`}><a>{article.title}</a></Link> - {article.abstract}
-        </li>
-      ))}
-    </ul>
-  </Layout>
-)
+class Index extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      articles: [],
+    }
+  }
+  render() {
+    return (
+       <Layout>
+        <h1>Articles</h1>
+        <ul>
+          {this.state.articles.map((article) => (
+            <li key={article.id}>
+              <Link as={`/a/${article.url_slug}`} href={`/article?slug=${article.url_slug}`}><a>{article.title}</a></Link> - {article.abstract}
+            </li>
+          ))}
+        </ul>
+      </Layout>
+    )
+  }
 
-Index.getInitialProps = async function() {
-  const res = await fetch(`https://${window.location.host}/api/articles`)
-  const data = await res.json()
-
-  console.log(`Show data fetched. Count: ${data.length}`)
-
-  return {
-    articles: data
+  componentDidMount() {
+    fetch(`https://${window.location.host}/api/articles`).then((res) => {
+      return res.json()
+    }).then((data) => {
+      console.log(`Show data fetched. Count: ${data.length}`)
+      this.setState({ articles: data })
+    })
   }
 }
 
